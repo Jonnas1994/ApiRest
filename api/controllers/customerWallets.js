5b243fdad94e95e8a05d08ece31cfda960a31759
Jonnas <jjonnas.ribeiro@gmail.com>
2021-07-01 21:37:01 -0300

    Adicionado swagger

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -5,6 +5,10 @@ module.exports = app => {
     controller.listCustomerWallets = (req, res) => {
         res.status(200).json(customerWalletsDB)
     };
+
+    controller.insertCustomerWallets = (req, res) => {
+        res.status(200).json(customerWalletsDB)
+    };
   
     return controller;
   }
--- api/routes/customerWallets.js
+++ api/routes/customerWallets.js
@@ -3,6 +3,18 @@ const config      = require('config');
 module.exports = app => {
     const controller = app.controllers.customerWallets;
 
+    /**
+    * @swagger
+    * /api/v1/customer-wallets:
+    *  get:
+    *    tags:
+    *       - name: Ferramentas
+    *    description: listar todos os customer-wallets
+    *    responses:
+    *      '200':
+    *        description: A successful response
+    */
     app.route( config.get('server.urlBase') + '/customer-wallets' )
         .get(controller.listCustomerWallets);
+
 }
