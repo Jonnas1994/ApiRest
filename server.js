c0950ea486df4e43c5622c0a264c32676a31c17a
Jonnas <jjonnas.ribeiro@gmail.com>
2021-08-10 19:00:36 -0300

    rw

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./config/express')();
 const port = app.get('port');
 
 app.listen(port, () => {
-  console.log(` yo! Servidor rodando na porta ${port}`)
+  console.log(` Yo! Servidor rodando na porta ${port}`)
 });
